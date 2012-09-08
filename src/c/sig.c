8dcd393023758ecbb5d75c6432f29f932fc3516d
Eric Schulte <eric.schulte@gmx.com>
2012-09-08 12:54:02 -0600

    starting to play with the gpgme library

--- src/c/sig.c
+++ src/c/sig.c
@@ -1,7 +1,26 @@
+#include <stdlib.h>
+#include <locale.h>
 #include <gpgme.h>
+
+void bail(gpgme_error_t err, const char * msg){
+  if(err){
+    printf("%s: [error] %s\n", msg, gpgme_strerror(err));
+    exit(err); } }
+
+void init(){
+  setlocale (LC_ALL, "");
+  gpgme_check_version (NULL);
+  gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
+  #ifdef LC_MESSAGES
+  gpgme_set_locale (NULL, LC_MESSAGES, setlocale (LC_MESSAGES, NULL));
+  #endif
+  bail(gpgme_engine_check_version(GPGME_PROTOCOL_OpenPGP),
+       "Initializing the engine"); }
 
 int main(int argc, char *argv[])
 {
-  printf("hello from %s(#%d) crypto\n", argv[0], argc);
-  return 0;
-}
+  gpgme_ctx_t ctx;
+  init();
+  bail(gpgme_new(&ctx), "Creating a context");
+  printf("success: %s(#%d)\n", argv[0], argc);
+  return 0; }
