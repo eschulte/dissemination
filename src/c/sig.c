7c78f96de3b271d6b2e9e854d5b8030dd48d17e1
Eric Schulte <eric.schulte@gmx.com>
2012-10-02 18:43:00 -0600

    still bad signature when loaded from a file

--- src/c/sig.c
+++ src/c/sig.c
@@ -2,6 +2,7 @@
 #include <locale.h>
 #include <gpgme.h>
 #include <string.h>
+#include <fcntl.h>
 
 void bail(gpgme_error_t err, const char * msg){
   if(err){
@@ -20,7 +21,13 @@ void init(){
 
 void str_to_data(gpgme_data_t *data, const char* string){
   bail(gpgme_data_new_from_mem(data, string, strlen(string), 1),
-       "creating a data buffer in memory");
+       "creating a data buffer from memory");
+}
+
+void file_to_data(gpgme_data_t *data, const char* file_name){
+  int fd = open(file_name, O_RDONLY);
+  bail(gpgme_data_new_from_fd(data, fd),
+       "creating a data buffer from a file");
 }
 
 int main()
@@ -29,6 +36,7 @@ int main()
   gpgme_data_t SIG, CONTENT;
   gpgme_verify_result_t result;
   gpgme_signature_t sig;
+  /*
   const char* sig_str = "-----BEGIN PGP SIGNATURE-----\n"
     "Version: GnuPG v2.0.19 (GNU/Linux)\n"
     "\n"
@@ -39,7 +47,8 @@ int main()
     "nOswdATDkDkQIl+maVsbrW0jJO6Dqp9RxjdKygObeIY7r8xWiqGdto2oV+4FFDQO\n"
     "OyU4jzhtDpGa+jeU8BGRnveiWQgn3Q0NBQnQmrt4rrbKcZAWbsEsfgkCFZhzqYI=\n"
     "=FGNI\n"
-    "-----END PGP SIGNATURE-----";
+    "-----END PGP SIGNATURE-----\n";
+  */
   const char* content_str = "patton";
 
   /* setup */
@@ -49,22 +58,22 @@ int main()
   bail(gpgme_new(&ctx), "Creating a context");
 
   /* signature */
-  str_to_data(&SIG, sig_str);
-  
+  file_to_data(&SIG, "/tmp/sig");
+
   /* content */
   str_to_data(&CONTENT, content_str);
   
   /* verify */
-  bail(gpgme_op_verify(ctx, SIG, CONTENT, NULL), "verifying a signature");
+  bail(gpgme_op_verify(ctx, SIG, CONTENT, NULL), "verifying");
 
   /* view the result */
   result = gpgme_op_verify_result (ctx);
   sig = result->signatures;
   do {
   if (sig->status == GPG_ERR_NO_ERROR)
-    printf("verification succeeded\n");
+    printf("verified\n");
   else
-    printf("verification failed: [error] %s\n", gpgme_strerror(sig->status));
+    printf("not verified: %s\n", gpgme_strerror(sig->status));
   } while (sig->next);
 
   return 0;
