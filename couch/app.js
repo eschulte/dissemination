be0fd66d9d03f054b48e0a998568a616002358b0
Eric Schulte <eric.schulte@gmx.com>
2012-12-02 11:39:38 -0700

    cleanup couchdb app.js

--- couch/app.js
+++ couch/app.js
@@ -9,30 +9,38 @@ var ddoc = {
 
 module.exports = ddoc;
 
-// http://localhost:5984/foo/_design/app/_view/date
-ddoc.views.date = {
-  map: function(doc) {
-    if(doc.date && doc.content){
-      emit(doc.date, doc.content);
-    }
-  }
-}
+
+/// Views (return documents)
 
-// http://localhost:5984/foo/_design/app/_list/dd/date
-ddoc.lists.dd = function(doc, req) {
+// All documents -- http://localhost:5984/foo/_design/app/_view/all
+ddoc.views.all = {
+  map: function(doc) { emit(doc._id, doc); } }
+
+// With dates -- http://localhost:5984/foo/_design/app/_view/date
+ddoc.views.date = {
+  map: function(doc) { if (doc.date) { emit(doc.date, doc)}; } }
+
+
+/// Lists (generate html from a view)
+
+// http://localhost:5984/foo/_design/app/_list/w_link/all
+// http://localhost:5984/foo/_design/app/_list/w_link/date
+ddoc.lists.w_link = function(doc, req) {
   start({ headers: {"Content-type": "text/html"} });
   send("<dl>\n");
   while(row = getRow()) {
-    send("\t<dt>" + row.key + "</dt>\n");
-    send("\t<dd>" + row.value + "</dd>\n");
-  }
-  send("</dl>\n")
-}
+    send("\t<dt><a href=\"../../_show/it/" + row.value._id + "\">"+row.key+"</a></dt>\n");
+    if (row.value.content) { send("\t<dd>" + row.value.content + "</dd>\n"); }
+    else { send("\t<dd>--</dd>\n"); } }
+  send("</dl>\n"); }
+
+
+/// Shows (view a document)
 
 // http://localhost:5984/foo/_design/app/_show/it/test
 ddoc.shows.it = function(doc, req) {
+  if (doc.content) { body = "<p id='thing' class='content'>" + doc.content + "</p>\n"; }
+  else { body = "<p>no content</p>"; }
   return {
     headers: {"Content-type": "text/html"},
-    body: "<p id='thing' class='content'>" + doc.content + "</p>\n"
-  }
-}
+    body: body }; }
